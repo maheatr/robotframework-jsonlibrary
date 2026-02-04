e6329908da5f088c12791c147ae1d861a011893b
maheatr <148945823+maheatr@users.noreply.github.com>
2026-02-04 07:31:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     url="https://github.com/maheatr/robotframework-jsonlibrary.git",
     packages=["JSONLibrary"],
     package_dir={"robotframework-jsonlibrary": "JSONLibrary"},
-    install_requires=requirements,
+    install_requires=[jsonpath-ng@git+https://github.com/h2non/jsonpath-ng.git],
     include_package_data=True,
     keywords="testing robotframework json jsonschema jsonpath",
     classifiers=[
